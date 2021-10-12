7c3c1fc67900f139b546d7acef445eefb8c85b00
nickskills <81989910+nickskills@users.noreply.github.com>
2021-10-12 16:00:57 +0300

    Добавил мобильную версию и js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+const schemeSvg = document.querySelector(".scheme_svg");
+const totalPriceTag = document.querySelector(".price_total");
+const menuButton = document.querySelector(".m_menu");
+const menu = document.querySelector(".menu");
+let cost = 800;
+let totalPrice = 0;
+schemeSvg.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("booked")) {
+    event.target.classList.toggle("active");
+    let totalSeats = schemeSvg.querySelectorAll(".active").length;
+    totalPrice = totalSeats * cost;
+    totalPriceTag.textContent = totalPrice;
+  }
+});
+menuButton.addEventListener("click", () => {
+  console.log("Мы кликнули по меню");
+  menu.classList.toggle("is_open");
+})
